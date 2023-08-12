b5cdc3fff2a1b6016fdc45105460d1e5efb29299
Roni Torres <ronitls1989@gmail.com>
2023-08-11 21:34:34 -0300

    arquivo Router

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,19 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/tasks', (req, res) => {
+  res.status(200).send('Olá, mundo')
+});
+
+app.post('/tasks/index');
+
+app.put('/tasks/eu');
+
+app.delete('/tasks/ele');
+
+
+
+app.listen(3333, () => {
+  console.log('Sever is runing on port 3333')
+});
